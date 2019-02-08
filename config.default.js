9cbbdf97ec565051a1566c7e81e5a9e1bcd10f41
Jan Fiedler <janfiedler@gmail.com>
2019-02-08 23:30:43 +0100

    config.default updated

--- config.default.js
+++ config.default.js
@@ -1,39 +1,27 @@
 let config = {
-    development: {
+    exchanges: [{
+        name: "coinfalcon",
+        url: "https://coinfalcon.com",
+        active: true,
         debug: true,
-
-        coinfalcon: {
-            url: "https://coinfalcon.com",
-            CF_API_KEY: "",
-            CD_API_SECRET_KEY: "",
-            ignoreOrderSize: 10, // How big size of order is ignored
-            pipsAskBidSpread: 0.0010, // How big spread between ask/bid order and between first and second order
-            sleepPause: 10000, // How much ms wait before continue new loop
-        },
-        coinmate: {
-            privateKey: "",
-            publicKey: "",
-            clientId: ""
-        }
-    },
-    production: {
+        CF_API_KEY: "",
+        CD_API_SECRET_KEY: "",
+        sleepPause: 225, // How much ms wait before continue new loop
+        // ignoreOrderSize: How big size of order is ignored
+        // How big spread between ask/bid order and between first and second order
+        pairs: [{name: "IOT-EUR", separator: "-", digitsPrice: 4, digitsSize: 5, buyForAmount: 10, ignoreOrderSize: 1, pipsAskBidSpread: 10, percentageProfitTarget: 0.12, pipsBuySpread: 10}, {name: "BTC-EUR",separator: "-", digitsPrice: 2, digitsSize: 8, buyForAmount: 20, ignoreOrderSize: 0.0, pipsAskBidSpread: 100, percentageProfitTarget: 0.12, pipsBuySpread: 100}],
+        accounts: [{name:"eur"},{name:"btc"},{name:"iot"}]
+    },{
+        name: "coinmate",
+        url: "https://coinmate.io",
+        active: true,
         debug: true,
-        ignoreOrderSize: 10,
-        pipsAskBidSpread: 0.0010, // How big spread between ask/bid order and between first and second order
-        sleepPause: 10000, // How much ms wait before continue new loop
-        coinfalcon: {
-            url: "https://coinfalcon.com",
-            CF_API_KEY: "",
-            CD_API_SECRET_KEY: "",
-            ignoreOrderSize: 10, // How big size of order is ignored
-            pipsAskBidSpread: 0.0010, // How big spread between ask/bid order and between first and second order
-            sleepPause: 10000, // How much ms wait before continue new loop
-        },
-        coinmate: {
-            privateKey: "",
-            publicKey: "",
-            clientId: ""
-        }
-    }
+        privateKey: '',
+        publicKey: '',
+        clientId: '',
+        sleepPause: 625, // How much ms wait per api call before continue new loop. Coinmate: 100 request per minute
+        accounts: [{name:"BTC"},{name:"CZK"}],
+        pairs: [{name: "BTC_CZK", separator: "_", digitsPrice: 2, digitsSize: 4, buyForAmount: 50, ignoreOrderSize: 0.0001, pipsAskBidSpread: 2500, percentageProfitTarget: 0.5, pipsBuySpread: 10000}]
+    }]
 };
 module.exports = config;
