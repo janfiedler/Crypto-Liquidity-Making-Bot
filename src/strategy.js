450aedf0d2ffc7305c3098e98a7eea33813ff9bb
Jan Fiedler <janfiedler@gmail.com>
2019-03-03 23:34:06 +0100

    Log optimalization - performance (remove times and put only one to final logMessage) - prevent duplicity logMessage (log file dont grow so fast)

--- src/strategy.js
+++ src/strategy.js
@@ -1,4 +1,5 @@
 const tools = require('../src/tools');
+var crypto = require('crypto');
 
 let config;
 let myAccount;
@@ -6,12 +7,18 @@ let db;
 let api;
 let apiCounter = 0;
 let logMessage;
+let lastLogMessage = [];
 
 let init = function (configuration, balance, database, apiExchange){
     config = configuration;
     myAccount = balance;
     db = database;
     api = apiExchange;
+
+    for(let i=0;i<config.pairs.length;i++){
+        let pair = config.pairs[i];
+        lastLogMessage[pair.name] = {"ask": "", "bid": ""};
+    }
 };
 
 let doAskOrder = async function(){
@@ -21,15 +28,14 @@ let doAskOrder = async function(){
         apiCounter = 0;
         let pair = config.pairs[i];
         logMessage = "///////////////////////////// doAskOrder "+pair.name+" ////////////////////////////\n";
-        logMessage += JSON.stringify(myAccount)+"\n";
-        logMessage += new Date().toISOString()+" ### Lets process ask for "+ pair.name+" in the loop.\n";
+        logMessage += " ### Lets process ask for "+ pair.name+" in the loop.\n";
         //let sellingForCurrency = pair.name.split('-')[1];
         //let sellingCurrency = pair.name.split('-')[0];
 
         //Get lowest pending sell order
         const pendingSellOrder = await db.getLowestFilledBuyOrder(config.name, pair);
         if(!pendingSellOrder){
-            logMessage += new Date().toISOString()+" ### PendingSellOrder not found, skipp the loop.\n";
+            logMessage += " ### PendingSellOrder not found, skipp the loop.\n";
             //Nothing to sell, skip the loop.
             continue;
         }
@@ -51,7 +57,7 @@ let doAskOrder = async function(){
         let targetAsk = await findSpotForAskOrder(pendingSellOrder, tickers[pair.name] , pair);
 
         if(typeof resultOpenedSellOrder !== 'undefined' && resultOpenedSellOrder){
-            logMessage += new Date().toISOString()+" ### Found opened sell order " + resultOpenedSellOrder.sell_id + "\n";
+            logMessage += " ### Found opened sell order " + resultOpenedSellOrder.sell_id + "\n";
             if(targetAsk !== resultOpenedSellOrder.sell_price){
                 //If founded opened sell order, lets check and process
                 const resultValidateOrder = await validateOrder("SELL", resultOpenedSellOrder.sell_id, pair, resultOpenedSellOrder);
@@ -60,16 +66,23 @@ let doAskOrder = async function(){
                     await processAskOrder(pair, targetAsk, pendingSellOrder);
                 }
             } else {
-                logMessage += new Date().toISOString()+" ### We already have opened ask order at " + targetAsk + " skipping validateOrder\n";
+                logMessage += " ### We already have opened ask order at " + targetAsk + " skipping validateOrder\n";
             }
         } else {
-            logMessage += new Date().toISOString()+" !!! This will be first opened sell order!\n";
+            logMessage += " !!! This will be first opened sell order!\n";
             await processAskOrder(pair, targetAsk, pendingSellOrder);
         }
-        logMessage += new Date().toISOString()+" ### Success finished "+pair.name+" ASK task, wait: "+(config.sleepPause * apiCounter)+" ms\n";
+        logMessage += " ### Success finished "+pair.name+" ASK task, wait: "+(config.sleepPause * apiCounter)+" ms\n";
         logMessage += "//////////////////////////////////////////////////////////////////////////////\n";
-        config.debug && console.log(logMessage);
-        await tools.sleep(config.sleepPause * apiCounter);
+
+        const hasLogMessage = crypto.createHash('md5').update(logMessage).digest('hex');
+        if(lastLogMessage[pair.name].ask !== hasLogMessage){
+            config.debug && console.log(new Date().toISOString()+"\n"+JSON.stringify(myAccount)+"\n"+logMessage);
+            lastLogMessage[pair.name].ask = hasLogMessage;
+        }
+        if(apiCounter > 0){
+            await tools.sleep(config.sleepPause * apiCounter);
+        }
     }
     return true;
 };
@@ -81,8 +94,7 @@ let doBidOrder = async function (){
         apiCounter = 0;
         let pair = config.pairs[i];
         logMessage = "///////////////////////////// doBidOrder "+pair.name+" ////////////////////////////\n";
-        logMessage += JSON.stringify(myAccount)+"\n";
-        logMessage += new Date().toISOString()+" ### Lets process bid for "+ pair.name+" in the loop.\n";
+        logMessage += " ### Lets process bid for "+ pair.name+" in the loop.\n";
         //let buyForCurrency = pair.name.split(pair.separator)[1];
         //let buyCurrency = pair.name.split(pair.separator)[0];
 
@@ -113,7 +125,7 @@ let doBidOrder = async function (){
         }
 
         if(typeof resultOpenedBuyOrder !== 'undefined' && resultOpenedBuyOrder){
-            logMessage += new Date().toISOString()+" ### Found opened bid order " + resultOpenedBuyOrder.buy_id+"\n";
+            logMessage += " ### Found opened bid order " + resultOpenedBuyOrder.buy_id+"\n";
             if(targetBid !== resultOpenedBuyOrder.buy_price) {
                 //If founded opened buy order, lets check and process
                 const resultValidateOrder = await validateOrder("BUY", resultOpenedBuyOrder.buy_id, pair, resultOpenedBuyOrder);
@@ -122,16 +134,23 @@ let doBidOrder = async function (){
                     await processBidOrder(pair, targetBid);
                 }
             } else {
-                logMessage += new Date().toISOString()+" ### We already have opened bid order at " + targetBid + " skipping validateOrder\n";
+                logMessage += " ### We already have opened bid order at " + targetBid + " skipping validateOrder\n";
             }
         } else {
-            logMessage += new Date().toISOString()+" !!! This will be first opened buy order!\n";
+            logMessage += " !!! This will be first opened buy order!\n";
             await processBidOrder(pair, targetBid);
         }
-        logMessage += new Date().toISOString()+" ### Success finished "+pair.name+" BID task, wait: "+(config.sleepPause * apiCounter)+" ms\n";
+        logMessage += " ### Success finished "+pair.name+" BID task, wait: "+(config.sleepPause * apiCounter)+" ms\n";
         logMessage += "//////////////////////////////////////////////////////////////////////////////\n";
-        config.debug && console.log(logMessage);
-        await tools.sleep(config.sleepPause * apiCounter);
+
+        const hasLogMessage = crypto.createHash('md5').update(logMessage).digest('hex');
+        if(lastLogMessage[pair.name].bid !== hasLogMessage){
+            config.debug && console.log(new Date().toISOString()+"\n"+JSON.stringify(myAccount)+"\n"+logMessage);
+            lastLogMessage[pair.name].bid = hasLogMessage;
+        }
+        if(apiCounter > 0){
+            await tools.sleep(config.sleepPause * apiCounter);
+        }
     }
     return true
 };
@@ -147,7 +166,7 @@ let findSpotForAskOrder = async function (pendingOrder, ticker, pair){
                 break;
             }
             if(ticker.ask[i].size > (ticker.ask[(i+1)].size+ticker.ask[(i+2)].size) && ticker.ask[i].size > pendingOrder.sell_size){
-                logMessage += new Date().toISOString()+ " ### "+ticker.ask[i].price + " is my target price with size: " + ticker.ask[i].size+"\n";
+                logMessage += " ### "+ticker.ask[i].price + " is my target price with size: " + ticker.ask[i].size+"\n";
                 targetAsk = ticker.ask[i].price;
                 break;
             }
@@ -157,10 +176,10 @@ let findSpotForAskOrder = async function (pendingOrder, ticker, pair){
     //Validate if new target ask is not close to bid order or taking bid order.
     const bidBorderPipsSpreadFromAsk = tools.addPipsToPrice(ticker.bidBorder, pair.pipsAskBidSpread, pair.digitsPrice);
     if(targetAsk < bidBorderPipsSpreadFromAsk) {
-        logMessage += new Date().toISOString()+ "### New target ask "+targetAsk+" is in danger zone bid border "+ticker.bidBorder+", targetAsk = bidBorderPipsSpreadFromAsk: "+bidBorderPipsSpreadFromAsk+"\n";
+        logMessage += "### New target ask "+targetAsk+" is in danger zone bid border "+ticker.bidBorder+", targetAsk = bidBorderPipsSpreadFromAsk: "+bidBorderPipsSpreadFromAsk+"\n";
         targetAsk = bidBorderPipsSpreadFromAsk;
     } else {
-        logMessage += new Date().toISOString()+" targetAsk: " + targetAsk+"\n";
+        logMessage += " targetAsk: " + targetAsk+"\n";
     }
     return targetAsk;
 };
@@ -176,7 +195,7 @@ let findSpotForBidOrder = async function (firstOrder, lowestOrder, buyOrder, tic
                 break
             }
             if(ticker.bid[i].size > (ticker.bid[(i+1)].size+ticker.bid[(i+2)].size) && ticker.bid[i].size > buyOrder.buy_size){
-                logMessage += new Date().toISOString()+ " ### "+ticker.bid[i].price + " is my target price with size: " + ticker.bid[i].size+"\n";
+                logMessage += " ### "+ticker.bid[i].price + " is my target price with size: " + ticker.bid[i].size+"\n";
                 targetBid = ticker.bid[i].price;
                 break;
             }
@@ -188,7 +207,7 @@ let findSpotForBidOrder = async function (firstOrder, lowestOrder, buyOrder, tic
     if(lowestOrder){
         const bidWithSpread = tools.takePipsFromPrice( buyOrder.buy_price, pair.pipsBuySpread, pair.digitsPrice);
         if(targetBid > bidWithSpread){
-            logMessage += new Date().toISOString()+ " ### Target bid " +targetBid+" is higher than previous filled buy order with spread "+bidWithSpread+" included!\n";
+            logMessage += " ### Target bid " +targetBid+" is higher than previous filled buy order with spread "+bidWithSpread+" included!\n";
             targetBid = bidWithSpread;
         }
     }
@@ -196,10 +215,10 @@ let findSpotForBidOrder = async function (firstOrder, lowestOrder, buyOrder, tic
     //Validate if new target ask is not close to bid order or taking bid order.
     const askBorderPipsSpreadFromBid = tools.takePipsFromPrice(ticker.askBorder, pair.pipsAskBidSpread, pair.digitsPrice);
     if(targetBid > askBorderPipsSpreadFromBid) {
-        logMessage += new Date().toISOString()+ " ### New target bid "+targetBid+" is in danger zone of ask border "+ticker.askBorder+". Target bid = askBorderPipsSpreadFromBid: "+ askBorderPipsSpreadFromBid+"\n";
+        logMessage += " ### New target bid "+targetBid+" is in danger zone of ask border "+ticker.askBorder+". Target bid = askBorderPipsSpreadFromBid: "+ askBorderPipsSpreadFromBid+"\n";
         targetBid = askBorderPipsSpreadFromBid;
     }else {
-        logMessage += new Date().toISOString()+" targetBid: " + targetBid+"\n";
+        logMessage += " targetBid: " + targetBid+"\n";
     }
     return targetBid;
 };
@@ -210,24 +229,24 @@ async function validateOrder(type, id, pair, openedOrder){
     const canceledOrder = await api.cancelOrder(id, type, openedOrder);
     apiCounter++;
     if (canceledOrder.s){
-        logMessage += new Date().toISOString() + " ### orderDetail = api.cancelOrder(id)\n";
+        logMessage += " ### orderDetail = api.cancelOrder(id)\n";
         orderDetail = canceledOrder.data;
     } else if(!canceledOrder.s && canceledOrder.data.error.includes('not found')){
         //Order was probably canceled manually, sync local DB
         const detailOrder = await api.getOrder(id, type, openedOrder);
         apiCounter++;
         if(detailOrder.s){
-            logMessage += new Date().toISOString() + " ### orderDetail = api.getOrder(id)\n";
+            logMessage += " ### orderDetail = api.getOrder(id)\n";
             orderDetail = detailOrder.data;
         } else {
-            logMessage +=  new Date().toISOString() + " !!! Something bad happened when validate canceled order "+id+" !\n";
+            logMessage +=  " !!! Something bad happened when validate canceled order "+id+" !\n";
         }
     } else if(!canceledOrder.s && canceledOrder.data.error.includes('has wrong status.')){
         //Coinfalcon used to respond with this message if the order was not open anymore (fully filled or already cancelled). However they also respond with this (rarely) when the order is still actually open.
-        logMessage += new Date().toISOString() + " !!! Catched cancelOrder has wrong status\n";
+        logMessage += " !!! Catched cancelOrder has wrong status\n";
         return false;
     } else {
-        logMessage += new Date().toISOString() + " !!! Catched cancelOrder error\n";
+        logMessage += " !!! Catched cancelOrder error\n";
         return false;
     }
     logMessage += JSON.stringify(orderDetail)+"\n";
@@ -236,12 +255,12 @@ async function validateOrder(type, id, pair, openedOrder){
         // Order was not filled
         switch(orderDetail.type){
             case "BUY":
-                logMessage += new Date().toISOString() + " ### We have new price, old buy order was canceled\n";
+                logMessage += " ### We have new price, old buy order was canceled\n";
                 myAccount.available[pair.name.split(pair.separator)[1]] += orderDetail.funds;
                 await db.deleteOpenedBuyOrder(orderDetail.id);
                 break;
             case "SELL":
-                logMessage += new Date().toISOString() + " ### We have new price, old sell order was canceled\n";
+                logMessage += " ### We have new price, old sell order was canceled\n";
                 myAccount.available[pair.name.split(pair.separator)[0]] += orderDetail.size;
                 await db.deleteOpenedSellOrder(orderDetail.id);
                 break;
@@ -275,14 +294,14 @@ async function validateOrder(type, id, pair, openedOrder){
         await processPartiallyFilled(pair, orderDetail);
         return false;
     } else {
-        logMessage += new Date().toISOString()+" !!! Something bad happened when validateOrder "+orderDetail.id+" !\n";
+        logMessage += " !!! Something bad happened when validateOrder "+orderDetail.id+" !\n";
     }
 }
 
 let processFulfilledOrder = function(pair, orderDetail){
     switch(orderDetail.type){
         case "BUY":
-            logMessage += new Date().toISOString()+" BID fulfilled\n";
+            logMessage += " BID fulfilled\n";
             if(orderDetail.fee > 0){
                 switch (config.name) {
                     case "coinfalcon":
@@ -302,7 +321,7 @@ let processFulfilledOrder = function(pair, orderDetail){
             myAccount.balance[pair.name.split(pair.separator)[1]] -= orderDetail.funds;
             break;
         case "SELL":
-            logMessage += new Date().toISOString()+" ### ASK fulfilled\n";
+            logMessage += " ### ASK fulfilled\n";
             if(orderDetail.fee > 0){
                 switch (config.name) {
                     case "coinfalcon":
@@ -328,7 +347,7 @@ let processFulfilledOrder = function(pair, orderDetail){
 let processPartiallyFilled = function (pair, orderDetail){
     switch(orderDetail.type){
         case "BUY":
-            logMessage += new Date().toISOString()+" BID partially_filled\n";
+            logMessage += " BID partially_filled\n";
             if(orderDetail.fee > 0){
                 switch (config.name) {
                     case "coinfalcon":
@@ -350,7 +369,7 @@ let processPartiallyFilled = function (pair, orderDetail){
             myAccount.available[pair.name.split(pair.separator)[1]] += ((orderDetail.size-orderDetail.size_filled)*orderDetail.price);
             break;
         case "SELL":
-            logMessage += new Date().toISOString()+" ### ASK partially_filled\n";
+            logMessage += " ### ASK partially_filled\n";
             if(orderDetail.fee > 0){
                 switch (config.name) {
                     case "coinfalcon":
@@ -378,13 +397,13 @@ let processPartiallyFilled = function (pair, orderDetail){
 
 async function processAskOrder(pair, targetAsk, pendingSellOrder){
     if(targetAsk === 0){
-        logMessage += new Date().toISOString()+" !!! Skipping process ask order because targetAsk === 0!\n";
+        logMessage += " !!! Skipping process ask order because targetAsk === 0!\n";
         return false;
     } else if (myAccount.available[pair.name.split(pair.separator)[0]] < pendingSellOrder.sell_size) {
-        logMessage += new Date().toISOString() + " !!! No available " + pair.name.split(pair.separator)[0] + " funds!\n";
+        logMessage += " !!! No available " + pair.name.split(pair.separator)[0] + " funds!\n";
         return false;
     } else if (pendingSellOrder.sell_target_price <= targetAsk) {
-        logMessage += new Date().toISOString()+" ### Let´go open new sell order!\n";
+        logMessage += " ### Let´go open new sell order!\n";
         const createdOrder = await api.createOrder(pair, "SELL", pendingSellOrder, targetAsk);
         apiCounter++;
         if(createdOrder.s){
@@ -395,25 +414,25 @@ async function processAskOrder(pair, targetAsk, pendingSellOrder){
             if(createdOrder.errorMessage.includes("insufficient size")){
                 const failedSellOrder = {"id": pendingSellOrder.buy_id, "status": "insufficient_size"};
                 await db.setFailedSellOrder(failedSellOrder);
-                logMessage += new Date().toISOString() + " !!! Sell order "+pendingSellOrder.buy_id+" finished due to insufficient order size!\n";
+                logMessage += " !!! Sell order "+pendingSellOrder.buy_id+" finished due to insufficient order size!\n";
             }
             return false;
         }
     } else {
-        logMessage += new Date().toISOString() + " !!! No sell order for this ask price!\n";
+        logMessage += " !!! No sell order for this ask price!\n";
         return false;
     }
 }
 
 async function processBidOrder(pair, targetBid){
     if(targetBid === 0){
-        logMessage += new Date().toISOString()+" !!! Skipping process bid order because targetBid === 0!\n";
+        logMessage += " !!! Skipping process bid order because targetBid === 0!\n";
         return false;
     } else if (myAccount.available[pair.name.split(pair.separator)[1]] < tools.setPrecisionUp((tools.getBuyOrderSize(pair, targetBid)*targetBid), pair.digitsPrice)){
-        logMessage += new Date().toISOString()+" !!! No available "+pair.name.split(pair.separator)[1]+" funds!\n";
+        logMessage += " !!! No available "+pair.name.split(pair.separator)[1]+" funds!\n";
         return false;
     } else {
-        logMessage += new Date().toISOString()+" ### Let´go open new buy order!\n";
+        logMessage += " ### Let´go open new buy order!\n";
         const createdOrder = await api.createOrder(pair,"BUY",null, targetBid);
         apiCounter++;
         if(createdOrder.s){
@@ -421,7 +440,7 @@ async function processBidOrder(pair, targetBid){
             await db.saveOpenedBuyOrder(config.name, pair, createdOrder);
             return true;
         } else {
-            logMessage += new Date().toISOString()+" !!! Order not opened!\n";
+            logMessage += " !!! Order not opened!\n";
             return false;
         }
     }
