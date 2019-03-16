b132c66abffe3ca4546ea27f3753c0147abd2d1c
Jan Fiedler <janfiedler@gmail.com>
2019-03-16 21:56:44 +0100

    websocket lib optimalization performance optimalization: fetch data from db and do math on pending sell orders, only when client is connected to websocket.

--- src/exchanges/coinmate/index.js
+++ src/exchanges/coinmate/index.js
@@ -1,14 +1,11 @@
 let cp = require('child_process');
 let coinmateWorker = cp.fork('src/exchanges/coinmate/worker.js');
 let ws = require('./../../websocket');
-let tools = require('./../../tools');
 
 let config;
-let db;
 
-let start = function(configuration, database){
+let start = function(configuration){
     config = configuration;
-    db = database;
     coinmateWorker.send({"type": "init", "config": config});
 };
 
@@ -38,14 +35,7 @@ coinmateWorker.on('message', async function (data) {
             coinmateWorker.kill();
             break;
         case "ticker":
-            const po = await db.getAllPendingOrders(data.exchange, data.pair);
-            const dailyProfit = await db.sumProfit(data.exchange, data.pair, new Date().toISOString().substr(0,10)+"%");
-            let pendingOrders = [];
-            for(let i=0;i<po.length;i++){
-                const pl = tools.calculatePendingProfit(po[i].exchange, po[i], data.tick.bid);
-                pendingOrders.push({"buy_id": po[i].buy_id, "buy_price": po[i].buy_price, "sell_size": po[i].sell_size, "sell_target_price": po[i].sell_target_price, "pl": pl});
-            }
-            ws.emitToAll("ticker", {"exchange": data.exchange, "pair": data.pair, "tick": data.tick, "dailyProfit": dailyProfit, "pendingOrders": pendingOrders});
+            ws.emitPendingOrders(data);
             break;
     }
 });
--- src/websocket.js
+++ src/websocket.js
@@ -2,6 +2,8 @@ let socket_io = require('socket.io');
 let io = socket_io();
 let websocket = {};
 let sockets = [];
+let tools = require('./tools');
+let db = require('../db/sqlite3');
 
 websocket.io = io;
 
@@ -16,7 +18,20 @@ let removeSocket = function (socket){
     }
 };
 
-websocket.emitToAll = function(event, data){
+websocket.emitPendingOrders = async function(data){
+    if(sockets.length > 0){
+        const po = await db.getAllPendingOrders(data.exchange, data.pair);
+        const dailyProfit = await db.sumProfit(data.exchange, data.pair, new Date().toISOString().substr(0,10)+"%");
+        let pendingOrders = [];
+        for(let i=0;i<po.length;i++){
+            const pl = tools.calculatePendingProfit(po[i].exchange, po[i], data.tick.bid);
+            pendingOrders.push({"buy_id": po[i].buy_id, "buy_price": po[i].buy_price, "sell_size": po[i].sell_size, "sell_target_price": po[i].sell_target_price, "pl": pl});
+        }
+        emitToAll("ticker", {"exchange": data.exchange, "pair": data.pair, "tick": data.tick, "dailyProfit": dailyProfit, "pendingOrders": pendingOrders});
+    }
+};
+
+let emitToAll = function(event, data){
     if(sockets !== undefined) {
         sockets.forEach(function (socketSingle){
             socketSingle.emit(event, data);
