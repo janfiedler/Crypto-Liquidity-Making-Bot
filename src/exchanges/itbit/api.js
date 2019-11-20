4bc9305c43c9cfa867b33e15c0d1c334dc67a1cf
Jan Fiedler <janfiedler@gmail.com>
2019-11-20 14:13:49 +0100

    roundPriceToQuarter need round down for sell and round up for buy

--- src/exchanges/itbit/api.js
+++ src/exchanges/itbit/api.js
@@ -279,7 +279,15 @@ let parseTicker = function(type, book, pair, order){
 let createOrder = async function(pair, type, pendingSellOrder, valueForSize, price){
     let size = "";
     if(pair.moneyManagement.roundPriceToQuarter){
-        price = (Math.round(price * 4) / 4).toFixed(2);
+        switch(type){
+            case "BUY":
+                price = (Math.ceil(price * 4) / 4).toFixed(2);
+                break;
+            case "SELL":
+                price = (Math.floor(price * 4) / 4).toFixed(2);
+                break;
+        }
+
     }
     switch(type){
         case "BUY":
